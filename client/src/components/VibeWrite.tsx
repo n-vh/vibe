a47bc8ca36697b58ce8444b58d0da4d267a14cd0
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-03-24 15:04:17 +0100

    send a vibe

--- client/src/components/VibeWrite.tsx
+++ client/src/components/VibeWrite.tsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useMutation } from 'urql';
 import Button from './Button';
 
 const VibeWrite: React.FC = () => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-  const [value, setValue] = useState<String>();
+  const [value, setValue] = useState<string>('');
   const textAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(event.target.value);
   };
+  const [data, execute] = useMutation(`
+  mutation CreateVibe($message: String!){
+    createVibe(message: $message) {
+      id
+      message
+      createdAt
+    }
+  }
+  `);
+
+  const sendVibe = () => {
+    execute({ message: value });
+    setValue('');
+  };
+
   useEffect(() => {
     if (textareaRef && textareaRef.current) {
       textareaRef.current.style.height = '0px';
@@ -24,11 +40,12 @@ const VibeWrite: React.FC = () => {
         id="vibe"
         ref={textareaRef}
         onChange={textAreaChange}
+        value={value}
         placeholder="send some positive vibes into the world..."
         className="w-[300px] resize-none bg-transparent font-roboto font-light tracking-wider sm:text-sm md:w-[375px] md:pt-3 md:text-base "
       ></textarea>
       <div className="relative flex">
-        <Button className="flex self-end">
+        <Button className="flex self-end" onClick={sendVibe}>
           <img src="/send.svg" alt="send" className="h-[30px] w-full md:h-[35px]"></img>
         </Button>
       </div>
