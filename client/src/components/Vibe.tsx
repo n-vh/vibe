30dc571a3a6c3ee0f447dfa6c02a75e7afcb2da1
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-03-27 18:24:42 +0200

    reply vibe design

--- client/src/components/Vibe.tsx
+++ client/src/components/Vibe.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 import { getTimeString, pluralString } from '../utils/format';
+import { useAuthContext } from '../hooks';
 
 interface VibeProps {
   avatar: string;
@@ -10,6 +11,7 @@ interface VibeProps {
   smileCount: number;
   message: string;
   commentCount: number;
+  openReplying?: boolean;
 }
 
 const Vibe: React.FC<VibeProps> = ({
@@ -19,26 +21,52 @@ const Vibe: React.FC<VibeProps> = ({
   smileCount,
   message,
   commentCount,
+  openReplying = false,
 }) => {
+  const { user } = useAuthContext();
+  const [value, setValue] = useState<string>('');
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const textAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    if (textareaRef && textareaRef.current) {
+      textareaRef.current.style.height = '0px';
+      const scrollHeight = textareaRef.current.scrollHeight;
+      textareaRef.current.style.height = scrollHeight + 'px';
+    }
+  }, [value]);
+
+  const [replying, setReplying] = useState(openReplying);
+
+  const toggleReply = () => {
+    setReplying(!replying);
+  };
+
   return (
     <div className="flex w-[355px] flex-shrink-0 flex-col gap-4 rounded-[16px] bg-white bg-opacity-90 p-5 shadow-md md:w-[500px] md:p-6">
+      {/* HEADER */}
+
       <div className="flex">
-        <img
-          src={`/avatars/${avatar}.svg`}
-          alt="avatar"
-          className="h-14 w-14 md:h-16 md:w-16 lg:h-14"
-        ></img>
+        <Link to={`/profile/${username}`}>
+          <img
+            src={`/avatars/${avatar}.svg`}
+            alt="avatar"
+            className="h-12 w-12 md:h-14 md:w-14"
+          ></img>
+        </Link>
 
         <div className="my-auto flex flex-col pl-4">
           <Link
             to={`/profile/${username}`}
-            className="font-roboto text-[18px] tracking-wider text-dark-grey md:text-xl lg:text-lg"
+            className="font-roboto text-[18px] tracking-wider text-dark-grey text-opacity-80 md:text-xl lg:text-lg"
           >
             {username}
           </Link>
           <time
             className="font-gothic text-dark-pink md:text-lg lg:text-base"
-            title={new Date(date).toString()}
+            title={new Date(+date).toString()}
           >
             {getTimeString(+date)}
           </time>
@@ -60,28 +88,72 @@ const Vibe: React.FC<VibeProps> = ({
         </div>
       </div>
 
+      {/* MESSAGE */}
+
       <div className="flex flex-wrap px-4">
         <p className="max-w-full break-words text-left font-roboto font-light tracking-wider md:text-lg lg:text-base">
           {message}
         </p>
       </div>
 
+      {/* BUTTONS */}
+
       <div className="flex items-center gap-4 px-4">
         <Button
-          className=" font-mincho text-[16px] text-dark-grey text-opacity-70 duration-200 hover:text-dark-pink md:text-lg lg:text-base"
+          className=" font-mincho text-[16px] text-dark-grey text-opacity-70 duration-100 hover:text-dark-pink md:text-lg lg:text-base"
           text={`${commentCount} ${pluralString(commentCount, 'message')}`}
         />
-        <div className="mx-2 h-4 w-[1px] bg-dark-grey bg-opacity-50"></div>
-        <Button
-          className="font-mincho text-[16px] text-dark-grey text-opacity-70 duration-200 hover:text-dark-pink md:text-lg lg:text-base"
-          text="reply"
-        />
-        <div className="mx-2 h-4 w-[1px] bg-dark-grey bg-opacity-50"></div>
-        <Button
-          className="font-mincho text-[16px] text-dark-grey text-opacity-70 duration-200 hover:text-error md:text-lg lg:text-base"
-          text="delete"
-        />
+
+        {!openReplying && (
+          <>
+            <div className="mx-2 h-4 w-[1px] bg-dark-grey bg-opacity-50"></div>
+            <Button
+              className="font-mincho text-[16px] text-dark-grey text-opacity-70 duration-100 hover:text-dark-pink md:text-lg lg:text-base"
+              text="reply"
+              onClick={toggleReply}
+            />
+          </>
+        )}
+
+        {user.username === username && (
+          <>
+            <div className="mx-2 h-4 w-[1px] bg-dark-grey bg-opacity-50"></div>
+            <Button
+              className="font-mincho text-[16px] text-dark-grey text-opacity-70 duration-100 hover:text-error md:text-lg lg:text-base"
+              text="delete"
+            />
+          </>
+        )}
       </div>
+
+      {(replying || openReplying) && (
+        <div>
+          <div className="my-2 h-[1px] w-full bg-dark-grey bg-opacity-30"></div>
+          <div className="mt-4 flex">
+            <div className="flex h-12 w-auto pr-3 md:h-14">
+              <img src={`/avatars/${user.avatar}.svg`} alt="avatar" />
+            </div>
+            <div className="flex flex-grow pt-1">
+              <textarea
+                id="vibe"
+                ref={textareaRef}
+                onChange={textAreaChange}
+                value={value}
+                placeholder="reply..."
+                className="w-full resize-none bg-transparent font-roboto font-light tracking-wider sm:text-sm md:text-base "
+              ></textarea>
+            </div>
+            <div className="flex">
+              <Button
+                className="flex self-end"
+                // onClick={}
+              >
+                <img src="/send.svg" alt="send" className="h-[30px] w-full md:h-[35px]" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
