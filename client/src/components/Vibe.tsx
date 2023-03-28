48f2c02c8dfaa92a057bdfaf22f38718b6cd9242
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-03-28 16:53:42 +0200

    Links in messages

--- client/src/components/Vibe.tsx
+++ client/src/components/Vibe.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './Button';
+import Message from './Message';
 import { getTimeString, pluralString } from '../utils/format';
 import { useAuthContext } from '../hooks';
 import { useMutation } from '../graphql';
@@ -144,7 +145,7 @@ mutation smileVibe($smileVibeId: ObjectID!) {
 
       <div className="flex flex-wrap px-4">
         <p className="max-w-full whitespace-pre-wrap break-words text-left font-roboto font-light tracking-wider md:text-lg lg:text-sm">
-          {message}
+          <Message text={message} />
         </p>
       </div>
 
