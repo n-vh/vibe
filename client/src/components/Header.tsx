80ce4807e32097f3dfb7c53f51a5427d83314a15
Nils <nils.vanhee@pm.me>
2023-03-29 09:19:58 +0200

    add animate.css

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 const Header: React.FC = () => {
   return (
     <div className="fixed flex w-full">
-      <img src="/wave.svg" className="h-[120px] w-full" />
+      <img
+        src="/wave.svg"
+        className="animate__animated animate__pulse header_wave h-[120px] w-full"
+      />
       <Link to="/">
         <img
           className="absolute top-1/2 left-1/2 h-[80px] -translate-x-1/2 -translate-y-1/2"
           src="/logo.svg"
-        ></img>
+        />
       </Link>
     </div>
   );
