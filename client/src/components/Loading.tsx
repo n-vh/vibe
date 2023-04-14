21bb00a618f5b6dc2fa6a09a3c8daad736b83595
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-04-14 11:42:27 +0200

    Loading animation

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Loading: React.FC = () => {
   return (
     <div className="mx-auto flex items-center gap-4 rounded-[16px] bg-white bg-opacity-60 p-4 px-6">
-      <img className="h-[30px] md:h-[40px]" src="/loading.svg" alt="loading"></img>
+      <img
+        className="h-[30px] animate-[spin_3s_linear_infinite] md:h-[40px]"
+        src="/loading.svg"
+        alt="loading"
+      ></img>
       <p className="font-roboto text-lg tracking-wider text-blue md:text-xl">Loading</p>
     </div>
   );
