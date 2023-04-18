ceb147aa93e85b1c0556ead79ef95e9213e51ca9
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-04-18 17:02:56 +0200

    Loading Home & Friends

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Vibe from '../components/Vibe';
 import LeftSidebar from '../components/LeftSidebar';
 import RightSidebar from '../components/RightSidebar';
 import { Title } from '../components/Title';
+import Skeleton from '../components/Skeleton';
 
 export function Home() {
   const [data] = useQuery({
@@ -16,22 +17,28 @@ export function Home() {
       <Title text="Home – vibe" />
       <LeftSidebar />
       <div id="inputs" className="mx-auto flex w-[355px] flex-col gap-6 md:w-[500px]">
-        <VibeWrite />
-        <div className="flex flex-col gap-6">
-          {data.data?.timeline?.vibes.map((vibe) => (
-            <Vibe
-              id={vibe.id}
-              key={`${vibe.id}`}
-              avatar={vibe.user.avatar}
-              username={vibe.user.username}
-              date={vibe.createdAt}
-              smileCount={vibe.smiles.count}
-              hasSmiled={vibe.smiles.hasSmiled}
-              message={vibe.message}
-              commentCount={vibe.replies.count}
-            />
-          ))}
-        </div>
+        {data.fetching ? (
+          <Skeleton />
+        ) : (
+          <>
+            <VibeWrite />
+            <div className="flex flex-col gap-6">
+              {data.data?.timeline?.vibes.map((vibe) => (
+                <Vibe
+                  id={vibe.id}
+                  key={`${vibe.id}`}
+                  avatar={vibe.user.avatar}
+                  username={vibe.user.username}
+                  date={vibe.createdAt}
+                  smileCount={vibe.smiles.count}
+                  hasSmiled={vibe.smiles.hasSmiled}
+                  message={vibe.message}
+                  commentCount={vibe.replies.count}
+                />
+              ))}
+            </div>
+          </>
+        )}
       </div>
 
       <RightSidebar />
